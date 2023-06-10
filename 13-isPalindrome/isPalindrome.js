4c98b4180ab895ee797ff76f18463963a2ae38d2
BrianShehu123 <105130740+BrianShehu123@users.noreply.github.com>
2023-06-09 22:19:02 -0400

    Update isPalindrome.js

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,9 @@
 function isPalindrome(word) {
   // Insert code here;
+  temp = word.split("").reverse().join("");
+  if(word === temp)
+    return true;
+  return false;
 }
 
 // Do not edit this line;
